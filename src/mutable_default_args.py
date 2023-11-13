1e78dafc05405e46bf45f5ce76ac0a7782619152
Ragu <88898517+Raguggg@users.noreply.github.com>
2023-11-13 05:41:40 +0000

    optimize code

--- src/mutable_default_args.py
+++ src/mutable_default_args.py
@@ -9,10 +9,21 @@ L2 = append_element(42) # [21, 42] - Oops..
 
 # Correct way 🔥: Use None ✅
 def better_append(elem, L=None):
-    if L is None:
+    if L:
         L = []
     L.append(elem)
     return L
 
 L1 = better_append(21) # [21]
 L2 = better_append(42) # [42]
+
+# Correct way 🔥: Use None ✅
+def better_append(elem, L=None):
+    # If L is None or evaluates to False (e.g., an empty list), initialize it to an empty list.
+    L = L or []
+
+    # Append the provided element to the list.
+    L.append(elem)
+
+    # Return the modified list.
+    return L
